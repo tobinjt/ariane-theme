8d9cc507bd20965c3d3667c0465e7c341e18c3ec
John Tobin <johntobin@johntobin.ie>
2012-03-29 00:27:42 +0200

    Fix title display and change formatting.

--- header.php
+++ header.php
@@ -9,21 +9,27 @@
 
 <?php function echo_title() {
   if (function_exists('is_tag') && is_tag()) {
-      single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
-  elseif (is_archive()) {
-      wp_title(''); echo ' Archive - '; }
-  elseif (is_search()) {
-      echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
-  elseif (!(is_404()) && (is_single()) || (is_page())) {
-      wp_title(''); echo ' - '; }
-  elseif (is_404()) {
-      echo 'Not Found - '; }
+      single_tag_title("Tag Archive for &quot;"); echo '&quot; - ';
+  } elseif (is_archive()) {
+      wp_title(''); echo ' Archive - ';
+  } elseif (is_search()) {
+      echo 'Search for &quot;'.wp_specialchars($s).'&quot; - ';
+  } elseif (!(is_404()) && (is_single()) || (is_page())) {
+      $title = wp_title('', False);
+      if ($title != '') {
+          echo $title, ' - ';
+      }
+  } elseif (is_404()) {
+      echo 'Not Found - ';
+  }
   if (is_home()) {
-      bloginfo('name'); echo ' - '; bloginfo('description'); }
-  else {
-      bloginfo('name'); }
+      bloginfo('name'); echo ' - '; bloginfo('description');
+  } else {
+      bloginfo('name');
+  }
   if ($paged>1) {
-      echo ' - page '. $paged; }
+      echo ' - page '. $paged;
+  }
 } ?>
 
 <head id="www-sitename-com" data-template-set="html5-reset-wordpress-theme">
