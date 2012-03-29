c1b36ff960f625e7c9fb9280c46e5d4c2dec3891
John Tobin <johntobin@johntobin.ie>
2012-03-29 23:33:26 +0200

    cp page.php page-home.php

---
+++ page-home.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+    <div id="main-content-page">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <article class="single-page" id="post-<?php the_ID(); ?>">
+
+                <!-- <h2><?php the_title(); ?></h2> -->
+
+                <div class="entry">
+
+                    <?php the_content(); ?>
+
+                    <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+
+                </div>
+
+                <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+
+            </article>
+
+        <?php endwhile; endif; ?>
+
+    </div>
+
+<?php get_footer(); ?>
