df6e109b8f6ca1147e4b19ae655fec918c75ed7a
John Tobin <johntobin@johntobin.ie>
2012-03-30 00:01:34 +0200

    Remove unused stuff.

--- page.php
+++ page.php
@@ -6,13 +6,9 @@
 
             <article class="single-page" id="post-<?php the_ID(); ?>">
 
-                <!-- <h2><?php the_title(); ?></h2> -->
-
                 <div class="entry">
 
                     <?php the_content(); ?>
-
-                    <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
 
                 </div>
 
