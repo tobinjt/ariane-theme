8e7a1c95a72a1d5de47d08c1829c03570392e756
John Tobin <johntobin@johntobin.ie>
2013-11-09 21:08:21 +0100

    Much saner formatting, plus some simplifications.

--- comments.php
+++ comments.php
@@ -1,19 +1,18 @@
 <?php
-
-  if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
+  if (!empty($_SERVER['SCRIPT_FILENAME'])
+      && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) {
     die ('Please do not load this page directly. Thanks!');
+  }
 
-  if ( post_password_required() ) { ?>
-    This post is password protected. Enter the password to view comments.
-  <?php
+  if (post_password_required()) {
+    echo '<p>This post is password protected.';
+    echo 'Enter the password to view comments.</p>';
     return;
   }
+
+  if (have_comments()) {
 ?>
-
-<?php if (have_comments()) { ?>
-
   <h2 id="comments"><?php comments_number('No Responses', 'One Response', '% Responses' );?></h2>
-
   <div class="navigation">
     <div class="next-posts"><?php previous_comments_link() ?></div>
     <div class="prev-posts"><?php next_comments_link() ?></div>
@@ -27,79 +26,83 @@
     <div class="next-posts"><?php previous_comments_link() ?></div>
     <div class="prev-posts"><?php next_comments_link() ?></div>
   </div>
+ <?php
+  }
 
- <?php } else { // this is displayed if there are no comments so far ?>
+  if (!comments_open()) {
+    echo '  <p>Comments are closed.</p>';
+    return;
+  }
+?>
+  <div id="respond">
+    <button id="click-to-respond">
+      <?php comment_form_title( 'Leave a Reply', 'Leave a Reply to %s' ); ?>
+    </button>
+    <button id="cancel-comment-form">
+      Hide comment form
+    </button>
+    <div id="comment-form-container">
+      <div class="cancel-comment-reply">
+        <?php cancel_comment_reply_link(); ?>
+      </div>
 
-  <?php if (!comments_open()) { // comments are closed ?>
-    <p>Comments are closed.</p>
-  <?php } ?>
+<?php
+  if (get_option('comment_registration') && !is_user_logged_in()) {
+?>
+      <p>You must be <a href="<?php echo wp_login_url(get_permalink());?>">
+      logged in</a> to post a comment.</p>'
+<?php
+  } else {
+?>
+      <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php"
+        method="post" id="commentform">
+<?php
+    if (is_user_logged_in()) {
+?>
+        <p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php">
+          <?php echo $user_identity; ?></a>.
+          <a href="<?php echo wp_logout_url(get_permalink()); ?>"
+            title="Log out of this account">Log out</a>
+        </p>
+<?php
+    } else {
+?>
+          <div>
+            <input type="text" name="author" id="author" size="22" tabindex="1"
+              value="<?php echo esc_attr($comment_author); ?>" />
+            <label for="author">Name </label>
+          </div>
 
-<?php } ?>
+          <div>
+            <input type="text" name="email" id="email"  size="22" "tabindex="2"
+              value="<?php echo esc_attr($comment_author_email); ?> />
+            <label for="email">EMail (will not be published) </label>
+          </div>
 
-<?php if (comments_open()) { ?>
+          <div>
+            <input type="text" name="url" id="url" size="22" tabindex="3"
+              value="<?php echo esc_attr($comment_author_url); ?>" />
+            <label for="url">Website</label>
+          </div>
+<?php
+    }
+?>
 
-<div id="respond">
+        <div>
+          <textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea>
+        </div>
 
-  <button id="click-to-respond">
-    <?php comment_form_title( 'Leave a Reply', 'Leave a Reply to %s' ); ?>
-  </button>
+        <div>
+          <input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
+          <?php comment_id_fields(); ?>
+        </div>
 
-  <button id="cancel-comment-form">
-    Hide comment form
-  </button>
+        <?php do_action('comment_form', $post->ID); ?>
 
-  <div id="comment-form-container">
-    <div class="cancel-comment-reply">
-      <?php cancel_comment_reply_link(); ?>
+      </form>
     </div>
 
-    <?php if (get_option('comment_registration') && !is_user_logged_in()) { ?>
-      <p>You must be <a href="<?php echo wp_login_url( get_permalink() ); ?>">logged in</a> to post a comment.</p>
-    <?php } else { ?>
-
-    <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-
-      <?php if (is_user_logged_in()) { ?>
-
-        <p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out</a></p>
-
-      <?php } else { ?>
-
-        <div>
-          <input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) { echo "aria-required='true'"; } ?> />
-          <label for="author">Name <?php if ($req) { echo "(required)"; } ?></label>
-        </div>
-
-        <div>
-          <input type="text" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) { echo "aria-required='true'"; } ?> />
-          <label for="email">Mail (will not be published) <?php if ($req) { echo "(required)"; } ?></label>
-        </div>
-
-        <div>
-          <input type="text" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="3" />
-          <label for="url">Website</label>
-        </div>
-
-      <?php } ?>
-
-      <!--<p>You can use these tags: <code><?php echo allowed_tags(); ?></code></p>-->
-
-      <div>
-        <textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea>
-      </div>
-
-      <div>
-        <input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
-        <?php comment_id_fields(); ?>
-      </div>
-
-      <?php do_action('comment_form', $post->ID); ?>
-
-    </form>
+<?php
+  } // If registration required and not logged in
+?>
   </div>
-
-  <?php } // If registration required and not logged in ?>
-
-</div>
-
-<?php } ?>
