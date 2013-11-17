4bbe6c6a02f91213aed667a57d6d998af5bae711
John Tobin <johntobin@johntobin.ie>
2013-11-17 18:49:40 +0100

    .set-width-from-image does not work unless the width is in the HTML.

--- footer.js
+++ footer.js
@@ -11,23 +11,3 @@ jQuery(document).ready(
     jQuery("#cancel-comment-form").click(toggle_comment_box);
   }
 );
-
-/* Set the width of an element based on the image inside it. */
-jQuery(document).ready(
-  function (){
-    jQuery('.set-width-from-image').each(
-      function() {
-        var images = jQuery(this).find('img');
-        var max_width = -1;
-        for (var i = 0; i < images.length; i++) {
-          var width = images[i].width;
-          if (width > max_width) {
-            max_width = width;
-          }
-        }
-        if (max_width > 0) {
-          jQuery(this).attr('style', 'max-width: ' + width + 'px');
-        }
-      });
-  }
-);
