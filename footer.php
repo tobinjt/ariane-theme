b70669bd4441b6dbee5d0701b9497cdbbe7ef536
John Tobin <johntobin@johntobin.ie>
2013-11-17 17:52:31 +0100

    Better formatting and comments.

--- footer.php
+++ footer.php
@@ -15,18 +15,21 @@ VALIDATOR;
 ?>
 
 <script type="text/javascript">
-/* trigger when page is ready */
-jQuery(document).ready(function (){
-  // Toggle the comment box to leave a reply.
-  var toggle_comment_box = function () {
-    jQuery("#comment-form-container").toggle();
-    jQuery("#cancel-comment-form").toggle();
-    jQuery("#click-to-respond").toggle();
+/* Set up toggling of the comment boxes. */
+jQuery(document).ready(
+  function (){
+    // Toggle the comment box to leave a reply.
+    var toggle_comment_box = function () {
+      jQuery("#comment-form-container").toggle();
+      jQuery("#cancel-comment-form").toggle();
+      jQuery("#click-to-respond").toggle();
+    }
+    jQuery("#click-to-respond").click(toggle_comment_box);
+    jQuery("#cancel-comment-form").click(toggle_comment_box);
   }
-  jQuery("#click-to-respond").click(toggle_comment_box);
-  jQuery("#cancel-comment-form").click(toggle_comment_box);
-});
+);
 
+/* Set the width of an element based on the image inside it. */
 jQuery(document).ready(
   function (){
     jQuery('.set-width-from-image').each(
