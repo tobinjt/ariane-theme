4d88c8982f6d426ddc182c501b893ebd2475aa02
John Tobin <johntobin@johntobin.ie>
2013-12-01 15:00:26 +0100

    Remove sidebar from 404 page.

--- 404.php
+++ 404.php
@@ -1,11 +1,9 @@
 <?php get_header(); ?>
 
   <div id="#main-content-post">
-    <h2><?php _e('Error 404 - Page Not Found', 'html5reset'); ?></h2>
+    <h2><?php _e('Page Not Found', 'html5reset'); ?></h2>
   </div>
 
 <?php
-  // TODO(johntobin): Should the sidebar be displayed here?
-  get_sidebar();
   get_footer();
 ?>
