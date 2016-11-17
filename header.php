c01ce147d336c09a1080e7ccc34fcf29b43c58e5
John Tobin <johntobin@johntobin.ie>
2016-11-17 23:49:56 +0100

    Use new favicons.

--- header.php
+++ header.php
@@ -26,8 +26,13 @@
     content="Copyright Ariane Tobin Jewellery 2011-<?php echo date("Y");?>. All Rights Reserved.">
   <meta name="dcterms.dateCopyrighted" content="2011-<?php echo date("Y");?>">
 
-  <link rel="shortcut icon"
-    href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_image_path("favicons/apple-touch-icon.png") ?>">
+  <link rel="icon" type="image/png" href="<?php echo get_image_path("favicons/favicon-32x32.png") ?>" sizes="32x32">
+  <link rel="icon" type="image/png" href="<?php echo get_image_path("favicons/favicon-16x16.png") ?>" sizes="16x16">
+  <link rel="manifest" href="<?php echo get_image_path("favicons/manifest.json") ?>">
+  <link rel="mask-icon" href="<?php echo get_image_path("favicons/safari-pinned-tab.svg") ?>" color="#5bbad5">
+  <meta name="theme-color" content="#ffffff">
+
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
