595495f5b8dc2225a246b4118db137f741d39f75
John Tobin <johntobin@johntobin.ie>
2017-05-01 19:05:00 +0200

    Fix styling of blog titles.

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
     the_post();
 ?>
       <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <h1 class="grey"><?php the_title(); ?></h1>
         <div class="entry-content">
 <?php
     the_content();
