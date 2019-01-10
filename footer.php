2398f20480364040abfe2c0d120a61c698988791
John Tobin <johntobin@johntobin.ie>
2019-01-10 23:11:06 +0000

    Use single quotes where possible.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <footer id="footer" class="source-org vcard copyright highlight">
       <ul>
-        <li>&copy;2011-<?php echo date("Y"); echo " ", strtolower(get_bloginfo('name')); ?></li>
+        <li>&copy;2011-<?php echo date('Y'); echo ' ', strtolower(get_bloginfo('name')); ?></li>
         <li><a class="highlight" href="tel:353868346825">phone: +353 86 834 6825</a></li>
         <li><a class="highlight" href="mailto:ariane@arianetobin.ie">email: ariane@arianetobin.ie</a></li>
       </ul>
