45703000de3f76beca3728d14910dbe115741124
John Tobin <johntobin@johntobin.ie>
2019-01-14 00:45:52 +0000

    Remove broken validator link.

--- footer.php
+++ footer.php
@@ -8,11 +8,6 @@
 
   </div>
 <?php
-  if (current_user_can('edit_pages')) {
-    echo <<<'VALIDATOR'
-    <div><a href="http://validator.w3.org/check?uri=referer">Validate</a></div>
-VALIDATOR;
-  }
   wp_footer();
 ?>
 
