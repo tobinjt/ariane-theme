e40596a2fd76bc1c7faf8acca50100c896b5fd5a
John Tobin <johntobin@johntobin.ie>
2021-10-24 18:27:36 +0100

    Remove single.php: 1) it is used to show blog posts but we do not have any, and 2) it is mostly a duplicate of index.php.

--- 404.php
+++ 404.php
@@ -1,8 +1,6 @@
 <?php get_header(); ?>
 
-  <div id="#main-content-post">
-    <h2><?php _e('Page Not Found', 'html5reset'); ?></h2>
-  </div>
+  <h2><?php _e('Page Not Found', 'html5reset'); ?></h2>
 
 <?php
   get_footer();
