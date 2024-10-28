d97ecda861d75a90b69adbe93dbba59904987da2
John Tobin <johntobin@johntobin.ie>
2024-10-28 23:20:58 +0000

    Remove unnecessary inline CSS.

--- functions.php
+++ functions.php
@@ -118,6 +118,7 @@ add_action('wp_enqueue_scripts', 'MaybeRemoveCookieLawInfoFromHead');
 function remove_wp_block_library_css(){
   wp_dequeue_style('wp-block-library');
   wp_dequeue_style('wp-block-library-theme');
+  wp_dequeue_style('global-styles');
 }
 add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
 
