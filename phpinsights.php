3087980685c7299926ee09fd07947bae8a7d17ab
John Tobin <johntobin@johntobin.ie>
2025-01-21 23:08:28 +0000

    fix(style): stop requiring classes to be in a namespace.

--- phpinsights.php
+++ phpinsights.php
@@ -10,6 +10,7 @@ use NunoMaduro\PhpInsights\Domain\Insights\CyclomaticComplexityIsHigh;
 use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenDefineFunctions;
 use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenGlobals;
 use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\CharacterBeforePHPOpeningTagSniff;
+use PHP_CodeSniffer\Standards\PSR1\Sniffs\Classes\ClassDeclarationSniff;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs\Classes\ValidClassNameSniff;
 use SlevomatCodingStandard\Sniffs\Classes\ForbiddenPublicPropertySniff;
 use SlevomatCodingStandard\Sniffs\Functions\FunctionLengthSniff;
@@ -22,12 +23,13 @@ return [
     'remove' => [
         // keep-sorted start
         CharacterBeforePHPOpeningTagSniff::class,
+        ClassDeclarationSniff::class,
         ForbiddenDefineFunctions::class,
         ForbiddenGlobals::class,
         ForbiddenPublicPropertySniff::class,
         ValidClassNameSniff::class,
+        // keep-sorted end
     ],
-    // keep-sorted end
 
     'config' => [
         CyclomaticComplexityIsHigh::class => [
